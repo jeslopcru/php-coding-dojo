3e3de812ab1aa35e99a3d877e5a1701e16b2ebfa
jeslopcru <jeslopcru@gmail.com>
2015-11-07 13:45:05 +0100

    refactor

--- Projects/TripServiceKataProject/test/TripServiceTest.php
+++ Projects/TripServiceKataProject/test/TripServiceTest.php
@@ -8,39 +8,73 @@ use TripServiceKata\User\User;
 
 class TripServiceTest extends PHPUnit_Framework_TestCase
 {
+    /** @var  User */
+    private $noFriendsUser;
+    /** @var  User */
+    private $withFriendsUser;
+    /** @var  User */
+    private $loggedUser;
+
+    public function setUp()
+    {
+        parent::setUp();
+        //Logged User
+        $this->loggedUser = new User('LoggedUserName');
+        //User with no Friends
+        $this->noFriendsUser = new User('AloneName');
+        $this->noFriendsUser->addTrip(new Trip('BackpackerTrip'));
+        //User with friend
+        $this->withFriendsUser = new User('FriendlyName');
+        $this->withFriendsUser->addFriend($this->loggedUser);
+        $this->withFriendsUser->addTrip(new Trip('FriendsWithFriends'));
+    }
+
     /** @test */
     public function ifUserNotLoggedThrowException()
     {
         $this->setExpectedException('TripServiceKata\Exception\UserNotLoggedInException');
         $tripService = new TripServiceKataCover(null);
-        $user = new User('Jesus');
-        $tripService->getTripsByUser($user);
+        $tripService->getTripsByUser($this->getAnyUserHelper());
+    }
+
+    /**
+     * @return User
+     */
+    private function getAnyUserHelper()
+    {
+        return new User('Foo User');
     }
 
     /** @test */
     public function whenUsersHasNoFriendsReturnAnEmptyList()
     {
-        $loggedUser = new User('LoggedUserName');
-        $tripService = new TripServiceKataCover($loggedUser);
-        $user = new User('Jesus');
+        $tripService = new TripServiceKataCover($this->loggedUser);
 
-        $result = $tripService->getTripsByUser($user);
+        $this->assertEquals([], $tripService->getTripsByUser($this->getNoFriendUserHelper()));
+    }
 
-        $this->assertEquals([], $result);
+    /**
+     * @return User
+     */
+    private function getNoFriendUserHelper()
+    {
+        return $this->noFriendsUser;
     }
 
     /** @test */
     public function whenUsersHasFriendsReturnTripListOfAFriend()
     {
-        $loggedUser = new User('LoggedUserName');
-        $jesus = new User('Jesus');
-        $jesus->addFriend($loggedUser);
-        $trip = new Trip();
-        $jesus->addTrip($trip);
+        $tripService = new TripServiceKataCover($this->loggedUser);
+        $result = $tripService->getTripsByUser($this->getWithFriendUserHelper());
 
-        $tripService = new TripServiceKataCover($loggedUser);
-        $result = $tripService->getTripsByUser($jesus);
+        $this->assertEquals($this->getWithFriendUserHelper()->getTrips(), $result);
+    }
 
-        $this->assertEquals([$trip], $result);
+    /**
+     * @return mixed
+     */
+    private function getWithFriendUserHelper()
+    {
+        return $this->withFriendsUser;
     }
 }
